0136284354acb076845afed8c4492dede4944255
C301-20\arsuser <hashimo10.ars@gmail.com>
2017-11-13 10:43:33 +0900

    MainActivity.java main.xml機能の追加
    登録ボタンを押したときに登録とトーストがでるようにしました。

--- app/src/main/java/takasi/sadamasasi/kokumahoiku/MainActivity.java
+++ app/src/main/java/takasi/sadamasasi/kokumahoiku/MainActivity.java
@@ -1,13 +1,33 @@
 package takasi.sadamasasi.kokumahoiku;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Hello!",Toast.LENGTH_SHORT).show();
+        Button Reg = new Button(this);
+
+        Reg = (Button)findViewById(R.id.entry_button);
+
+       Reg.setOnClickListener(this);
+
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"登録",Toast.LENGTH_SHORT).show(); //クリックされたらトーストを出すようにする
     }
 }
