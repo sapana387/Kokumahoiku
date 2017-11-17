ec56518a964a26b0732dee8b4f2010a429bb7305
C301-019\arsuser <ryopana3870@gmail.com>
2017-11-17 11:42:55 +0900

    見た目変更&遷移処理

--- app/src/main/java/takasi/sadamasasi/kokumahoiku/Sougei_kakunin.java
+++ app/src/main/java/takasi/sadamasasi/kokumahoiku/Sougei_kakunin.java
@@ -1,7 +1,10 @@
 package takasi.sadamasasi.kokumahoiku;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by arsuser on 2017/11/17.
@@ -12,5 +15,25 @@ public class Sougei_kakunin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.k3_2_sougei_kakunin);
+
+        Button button_back=(Button) findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Sougei_kakunin.this,Sougei_hoiku.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button_keltutei=(Button) findViewById(R.id.button_keltutei);
+        button_keltutei.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Sougei_kakunin.this,Kanryou.class);
+                startActivity(intent);
+            }
+        });
     }
 }
