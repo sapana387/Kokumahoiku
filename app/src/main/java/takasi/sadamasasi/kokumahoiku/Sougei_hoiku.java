868b04466577ad22181cf37fca4bf89a0b819778
C301-019\arsuser <ryopana3870@gmail.com>
2017-11-17 10:13:44 +0900

    見た目変更&遷移処理

--- app/src/main/java/takasi/sadamasasi/kokumahoiku/Sougei_hoiku.java
+++ app/src/main/java/takasi/sadamasasi/kokumahoiku/Sougei_hoiku.java
@@ -1,7 +1,10 @@
 package takasi.sadamasasi.kokumahoiku;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by arsuser on 2017/11/16.
@@ -13,6 +16,25 @@ public class Sougei_hoiku extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.k2_1_itizihoiku_yoyuku);
+        setContentView(R.layout.k3_1_sougei_hoiku);
+
+        Button button_back=(Button) findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Sougei_hoiku.this,Mypage.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button_keltutei=(Button) findViewById(R.id.button_keltutei);
+        button_keltutei.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Sougei_hoiku.this,Sougei_kakunin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/takasi/sadamasasi/kokumahoiku/Sougei_kakunin.java
@@ -0,0 +1,16 @@
+package takasi.sadamasasi.kokumahoiku;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by arsuser on 2017/11/17.
+ */
+
+public class Sougei_kakunin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.k3_2_sougei_kakunin);
+    }
+}
