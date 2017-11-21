e5e0b6f94385ecb73e966c347a249afa72333b3f
C301-019\arsuser <ryopana3870@gmail.com>
2017-11-21 14:44:50 +0900

    見た目変更&遷移処理

--- app/src/main/java/takasi/sadamasasi/kokumahoiku/Itizi_hoiku.java
+++ app/src/main/java/takasi/sadamasasi/kokumahoiku/Itizi_hoiku.java
@@ -1,18 +1,45 @@
 package takasi.sadamasasi.kokumahoiku;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+
+
+import java.util.Date;
+
+import static android.R.attr.id;
+
 
 /**
  * Created by arsuser on 2017/11/16.
  */
 
-public class Itizi_hoiku extends AppCompatActivity{
+public class Itizi_hoiku extends AppCompatActivity implements android.widget.CalendarView.OnDateChangeListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.k2_1_itizihoiku_yoyuku);
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(this);
+
+
+    }
+
+    @Override
+    public void onSelectedDayChange(@NonNull android.widget.CalendarView view, int year, int month, int dayOfMonth) {
+        Intent varIntent=new Intent(Itizi_hoiku.this,Itizi_time.class);
+
+        startActivity(varIntent);
     }
 }
+
+
+
--- app/src/main/java/takasi/sadamasasi/kokumahoiku/Itizi_time.java
+++ app/src/main/java/takasi/sadamasasi/kokumahoiku/Itizi_time.java
@@ -2,15 +2,17 @@ package takasi.sadamasasi.kokumahoiku;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by arsuser on 2017/11/16.
  */
 
-public class Itizi_time extends AppCompatActivity{
+public class Itizi_time extends AppCompatActivity {
 
 
     @Override
@@ -18,13 +20,25 @@ public class Itizi_time extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.k2_2_itizihoiku_time);
 
-        Button button_kakunin=(Button)findViewById(R.id.button_kakunin);
+        Button button_back=(Button) findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               Intent intent = new Intent(Itizi_time.this, Itizi_kakunin.class);
+                                               startActivity(intent);
+                                           }
+                                       });
+
+        Button button_kakunin = (Button) findViewById(R.id.button_kakunin);
         button_kakunin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Itizi_time.this,Itizi_kakunin.class);
+                Intent intent = new Intent(Itizi_time.this, Itizi_kakunin.class);
                 startActivity(intent);
+
             }
         });
+
+
     }
 }
